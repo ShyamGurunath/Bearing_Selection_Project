a9d3b0461f8d6fcc05b2dc56c5711fc55d1e2c74
ShyamGurunath <shyamgurunath474@gmail.com>
2020-11-29 03:18:26 +0530

    Edited the theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,9 +47,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_pdj_theme
-html_theme = 'sphinx_pdj_theme'
-html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+
+html_theme = 'pyramid'
+
 
 pygments_style = 'sphinx'
 
